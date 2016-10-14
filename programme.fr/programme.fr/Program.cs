0d8d28ae6efe6e32a86790dab4e2a4db30fd55d9
Isaac <iwalkercox11@gmail.com>
2016-10-14 19:28:53 +0100

    Initial files.

---
+++ programme.fr/programme.fr/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CSharp;
+
+namespace programme.fr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args[1] == "interactive")
+                {
+                    switch (args[2])
+                    {
+                        case "faire":
+                            System.Diagnostics.Process.Start(args[3]);
+                            break;
+                        case "ecoute":
+                            if (args[3] == "")
+                            {
+                                Console.ReadLine();
+                            }
+                            Console.WriteLine(args[3]);
+                            Console.ReadLine();
+                            break;
+                    }
+                }
+            } catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(Strings.ERROR_PRESS_KEY_TO_CONTINUE);
+                Console.Read();
+
+            }
+        }
+    }
+}
---
+++ programme.fr/programme.fr/Strings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programme.fr
+{
+        // translate these to french
+    public static class Strings
+    {
+         public static string ERROR_PRESS_KEY_TO_CONTINUE = "Press any key to continue...";
+    }
+}
